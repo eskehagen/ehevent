81a30cc51de34c2492ca5d08105d6ab697739caf
eskehagen <eskehagen@gmail.com>
2026-04-22 21:20:11 +0200

    Add @vercel/analytics package and integrate Analytics component in Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import { Navbar } from './Navbar';
 import { Footer } from './Footer';
 
@@ -11,6 +12,7 @@ export const Layout = () => {
         <Outlet />
       </main>
       <Footer />
+      <Analytics />
     </div>
   );
 };
